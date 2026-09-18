1e29ca9c5d7c6c2be057a6479a40dd0156f209b4
mariagoliveira10-prog <maria.g.oliveira10@aluno.senai.br>
2026-09-18 08:19:11 -0300

    feat(documento): correção de erros

--- DEVLAB_BACK_API/AppFront/Documento/Documento.js
+++ DEVLAB_BACK_API/AppFront/Documento/Documento.js
@@ -1,5 +1,5 @@
-const URL_API = 'http://Localhost:7881/api/v1/Documento';
-async function enviarDcumento() {
+const URL_API = 'https://localhost:7081/api/v1/Documento';
+async function enviarDocumento() {
     const codigoCliente = document.getElementById("codigoCliente").value;
     const intputArquivo = document.getElementById("arquivo");
     const arquivo = intputArquivo.files[0];
@@ -11,16 +11,16 @@ async function enviarDcumento() {
     const dadosArquivo = new FormData();
     dadosArquivo.append("arquivo", arquivo);
 
-     const response = await fetch('${URL_API}/upload/${codigoCliente}', {
-        method: "APOST",
+    const response = await fetch(`${URL_API}/upload/${codigoCliente}`, {
+        method: "POST",
         body: dadosArquivo
-     })
-    if (response, ok) {
+    });
+
+    if (response.ok) {
         alert("Documento enviado com sucesso!");
         document.getElementById("codigoCliente").value = " ";
         document.getElementById("arquivo").value = " ";
     } else {
-        const erro = await response.json();
-        alert("Erro: " + (erro.message || "Falha ao enviar o documento"));
+        alert("Falha ao enviar o arquivo");
     }
 }
